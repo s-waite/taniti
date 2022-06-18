adb45307ab5292ced685b3e0f2188c2e11e0274f
s-waite <samwaite11@gmail.com>
2022-06-18 14:39:13 -0600

    updated attractions page

--- pages/attractions.jsx
+++ pages/attractions.jsx
@@ -2,8 +2,12 @@ import Layout from "../components/Layout";
 import PictureAndText from "../components/PictureAndText";
 
 function Attractions() {
+    // beaches
+    // day trips
+    // nightlife
     return (
         <Layout src="/hero/snorkeling.jpg" pageTitle="Attractions" textColor="black">
+            <PictureAndText side="left" src="/beach.jpg" alt="a beach" title="Beaches">Test</PictureAndText>
         </Layout>
     )
 }
