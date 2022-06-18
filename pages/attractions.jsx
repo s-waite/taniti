2b31abb8f8c948efa94571de353b5d1f063e30ad
s-waite <samwaite11@gmail.com>
2022-06-18 14:25:05 -0600

    added attractions page

---
+++ pages/attractions.jsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+import PictureAndText from "../components/PictureAndText";
+
+function Attractions() {
+    return (
+        <Layout src="/hero/snorkeling.jpg" pageTitle="Attractions" textColor="black">
+        </Layout>
+    )
+}
+
+export default Attractions;
