ed01275a8e5e94ef7f93bd4f5b212ebebcddfc43
s-waite <samwaite11@gmail.com>
2022-06-19 15:58:35 -0600

    fixed mismatched sizing on transportation page

--- pages/transportation.jsx
+++ pages/transportation.jsx
@@ -10,7 +10,7 @@ export default function Transportation() {
     return (
         <Layout src="/hero/plane.jpg" pageTitle="Transportation" textColor="black">
 
-            <div className="flex gap-6 pt-12">
+            <div className="grid grid-cols-2 gap-6 pt-12">
                 <div className="flex flex-col gap-5">
                     <h1 className='text-5xl font-["Amatic_SC"] text-shadow-md'>Getting To Taniti</h1>
                     <div className="text-xl font-['Josefin_Sans'] leading-relaxed font-thin">Most people get to Taniti by air, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
@@ -21,7 +21,7 @@ export default function Transportation() {
                 </div>
                 <div className="flex flex-col gap-5">
                     <h1 className='text-5xl font-["Amatic_SC"] text-shadow-md'>Getting Around Taniti</h1>
-                    <div className="text-xl font-['Josefin_Sans'] leading-relaxed font-thin">There are lots of ways to get around Taniti, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+                    <div className="text-xl font-['Josefin_Sans'] leading-relaxed font-thin">There are lots of ways to get around Taniti, including:</div>
                     <ul>
                         <li>Public Busses</li>
                         <li>Private Busses</li>
